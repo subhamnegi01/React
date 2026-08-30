b7030070e4e7b2abff62906d43e6976a27a08cd4
Subham Negi <negisubham210@gmail.com>
2026-08-30 21:58:32 +0530

    commit

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,13 @@
-const element = React.createElement('h1',{id:'first',className: 'Rahul',style: {backgroundColor: 'blue',fontSize: '30px',color: 'white'}},'Hello Coder Army');
-const element2 = React.createElement('h1',{id:'second',className: 'Rahul',style: {backgroundColor: 'blue',fontSize: '30px',color: 'white'}},'How are you');
+import React from "react";
+import ReactDOM from "react-dom/client"
 
+
+const element = React.createElement('h1',{key: "1", id:'first',className: 'Rahul',style: {backgroundColor: 'blue',fontSize: '30px',color: 'white'}},'Hello Coder Army');
+const element2 = React.createElement('h1',{key:"2", id:'second',className: 'Rahul',style: {backgroundColor: 'blue',fontSize: '30px',color: 'white'}},'How are you');
+console.log(element)
+
+
+const newElement = <h1>Hello Coder Army</h1>
 
 const div1 = React.createElement('div',{},[element, element2])
 
