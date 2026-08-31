e7e3d1cd0ca49921808910ddcf7c91d1529a8eaf
Subham Negi <negisubham210@gmail.com>
2026-08-31 23:48:38 +0530

    commit

---
+++ 03/first.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const element1 = <h1>Hello Coder Army</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("div"))
+root.render(element1)
